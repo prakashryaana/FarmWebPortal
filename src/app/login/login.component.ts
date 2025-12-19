1b33a014b6027e8fde7ba6be6ea72c23add68bb1
Aditya Deshmukh <adydeshmukh@gmail.com>
2025-12-19 16:36:04 +0530

    updated login component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,23 +1,32 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { WebAuthnService } from '../enable-fingerprint/web-authn.service';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule, FormGroup,Validators,FormControl } from '@angular/forms';
 import { AuthService } from './auth.service';
-import { MatCard, MatCardActions, MatCardContent, MatCardHeader, MatCardSubtitle, MatCardTitle } from '@angular/material/card';
-import { MatIcon } from '@angular/material/icon';
-import { MatFormField, MatLabel } from '@angular/material/form-field';
+import { MatCard, MatCardActions, MatCardContent, MatCardHeader, MatCardModule, MatCardSubtitle, MatCardTitle } from '@angular/material/card';
+import { MatIcon, MatIconModule } from '@angular/material/icon';
+import { MatFormField, MatFormFieldModule, MatLabel } from '@angular/material/form-field';
 import { AuthTokenService } from './auth-token.service';
+import { CommonModule } from '@angular/common';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-login',
-  imports:[FormsModule,MatCard,MatCardHeader,MatCardTitle,MatCardContent,MatIcon,MatCardSubtitle,MatFormField,MatLabel,MatCardActions],
+  imports:[MatCard,MatCardHeader,MatCardTitle,MatCardContent,MatIcon,MatCardSubtitle,MatFormField,MatLabel,MatCardActions
+    ,CommonModule,ReactiveFormsModule,RouterLink,MatCardModule,MatFormFieldModule,MatInputModule,MatButtonModule,MatIconModule,MatProgressSpinnerModule
+  ],
   templateUrl: './login.component.html'
 })
 export class LoginComponent {
   mobile = '';
   password = '';
+  isLoading = false;
+  showPassword = false;
   canUsePasskey = !!(window as any).PublicKeyCredential;
   message = '';
+  loginForm!: FormGroup;
 
   constructor(
     private webAuthn: WebAuthnService,
@@ -30,11 +39,20 @@ export class LoginComponent {
     }
   }
 
-  async loginWithPassword(){
-    this.authService.loginWithPassword(this.mobile, this.password).subscribe({
-      next: () => this.router.navigate(['/home-dashboard']),
-      error: err => this.message = err.error || 'Invalid credentials'
+  ngOnInit() {
+    this.loginForm = new FormGroup({
+      mobile: new FormControl('', [Validators.required, Validators.pattern(/^[6-9]\d{9}$/)]),
+      password: new FormControl('', [Validators.required, Validators.minLength(8)])
     });
+  }
+
+  async loginWithPassword(){
+    if (this.loginForm.valid){
+      this.authService.loginWithPassword(this.loginForm.get('mobile').value, this.loginForm.get('password').value).subscribe({
+        next: () => this.router.navigate(['/home-dashboard']),
+        error: err => this.message = err.error || 'Invalid credentials'
+      });
+    }
   }
 
   async enablePasskey() {
