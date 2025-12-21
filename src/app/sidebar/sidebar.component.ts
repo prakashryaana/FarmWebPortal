f31febd3043ecca66a278815a118c4934bee825b
Aditya Deshmukh <adydeshmukh@gmail.com>
2025-12-22 00:30:01 +0530

    Beautified - Sidebar & header

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,12 +1,35 @@
 // sidebar.component.ts
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
+
+interface MenuItem {
+  label: string;
+  route: string;
+  icon: string;
+}
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule, MatIconModule, MatTooltipModule],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
-export class SidebarComponent {}
+export class SidebarComponent {
+  isCollapsed = signal(false);
+
+  menuItems: MenuItem[] = [
+    { label: 'On-boarding', route: '/farm-registration', icon: 'business' },
+    { label: 'Setup Crop', route: '/crop-registration', icon: 'agriculture' },
+    { label: 'Add Activity', route: '/add-activity', icon: 'add_circle' },
+    { label: 'View Activities', route: '/list-activity', icon: 'list' },
+    { label: 'Manage Users', route: '/user-management', icon: 'group' },
+  ];
+
+  toggleSidebar() {
+    this.isCollapsed.update(value => !value);
+  }
+}
