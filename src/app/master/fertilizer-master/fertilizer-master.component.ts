f7cb0e34498ae77d1b76464a99f65eb780c84698
prakashryaana <prakashr.yaana@gmail.com>
2026-08-25 22:36:28 +0530

    qr code change in fertilizer and disease control master

--- src/app/master/fertilizer-master/fertilizer-master.component.ts
+++ src/app/master/fertilizer-master/fertilizer-master.component.ts
@@ -223,6 +223,44 @@ export class FertilizerMasterComponent implements OnInit {
     });
   }
 
+  downloadQRCode(item: InputCatalogItem) {
+    const typeValue = this.type === 'DISEASE_CONTROL' ? 'Disease Control' : 'Fertilizer';
+    const jsonText = JSON.stringify({ productName: item.name, type: typeValue });
+    const qrUrl = `https://api.qrserver.com/v1/create-qr-code/?size=250x250&data=${encodeURIComponent(jsonText)}`;
+
+    const canvas = document.createElement('canvas');
+    const ctx = canvas.getContext('2d');
+    if (ctx) {
+      const img = new Image();
+      img.crossOrigin = 'anonymous';
+      img.onload = () => {
+        canvas.width = 250;
+        canvas.height = 290;
+
+        // Draw white background
+        ctx.fillStyle = '#ffffff';
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+        // Draw QR Code
+        ctx.drawImage(img, 0, 0, 250, 250);
+
+        // Draw centered product name text
+        ctx.fillStyle = '#000000';
+        ctx.font = 'bold 16px sans-serif';
+        ctx.textAlign = 'center';
+        ctx.textBaseline = 'middle';
+        ctx.fillText(item.name, 125, 270);
+
+        // Trigger download
+        const link = document.createElement('a');
+        link.download = `${item.name.replace(/\s+/g, '_')}_QR.png`;
+        link.href = canvas.toDataURL('image/png');
+        link.click();
+      };
+      img.src = qrUrl;
+    }
+  }
+
   goBack() {
     this.router.navigate(['/crop-master']);
   }
